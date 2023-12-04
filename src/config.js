122afd03fa5da90bcac19a1305174e04acc9f7a9
Voskanyan20 <arpman9900@gmail.com>
2023-12-04 12:42:37 +0400

    minor added server

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // TODO: Update APP_ID with your application ID (can be found in in Wix Developers under Workspace/OAuth)
-const APP_ID = 'Enter your APP ID here';
+const APP_ID = '8dfa96a4-b0dd-40e6-bbc0-796681f90504';
 
 // TODO: Update PUBLIC_KEY with your public Key (can be found under Workspace/Webhooks/Public-Key)
-const PUBLIC_KEY = 'Enter your PUBLIC KEY here';
+const PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvggWNXfyXkJjzqF08xU4XFyHrcf7V4LLCZRZNiGpiI2CeW4gL8gNvAeKGSxD/uSPNOqstWMM7eGdQJyDgBSlE0HdF+2rrupyD9WPSasGHChGhBQQ+tcM3mKBVhh+wdzDARULk5kCcKZhlQJgLM9Xae28/fd61Fz28ndYRYh6k0tjHdIrkfd50JXpF7M3c+jkwsmmdO0b/lRilgDnS7V8blwpiAcpZ5w4smiPhMkwEpwNxDGVqxx5cnnBXwc0uVhHtypVTUG+D1EYTictDqokegCD1TahgC4QCaDUn7wUqcLsK97xcAMQCBbk1WMuukqZKU50PNpkFna4xnXFOuZTKwIDAQAB";
 
 exports.APP_ID = APP_ID;
 exports.PUBLIC_KEY = PUBLIC_KEY;
--- src/credentials.js
+++ src/credentials.js
@@ -1,4 +1,4 @@
 // TODO: Update APP_SECRET with your application secret key (can be found in Wix Developers under Workspace/OAuth)
-const APP_SECRET = 'App Secret Goes Here';
+const APP_SECRET = 'eb2ccf1e-8563-44c4-8392-cafc70e0cd0a';
 
 exports.APP_SECRET = APP_SECRET;
